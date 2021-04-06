df0439b81d4c4391fbace365b3d064a070358a72
kellybush <kelly@kellybush.me>
2021-04-06 11:35:56 -0500

    v.02
    updated commands

--- Get-Groups-Empty.ps1
+++ Get-Groups-Empty.ps1
@@ -3,21 +3,21 @@ $final_local = "$env:userprofile\Desktop\ADData\GroupData\";
 
 if(!$local.Equals("C:\"))
 {
-    cd "C:\";
+    Set-Location "C:\";
     if((Test-Path $final_local) -eq 0)
     {
         mkdir $final_local;
-        cd $final_local;
+        Set-Location $final_local;
     }
 
     ## if path already exists
     ## DB Connect
     elseif ((Test-Path $final_local) -eq 1)
     {
-        cd $final_local;
-        echo $final_local;
+        Set-Location $final_local;
+        Write-Output $final_local;
     }
 }
 $ErrorActionPreference = 'SilentlyContinue'
-Get-ADGroup -Filter {GroupCategory -eq 'Security'} | ?{@(Get-ADGroupMember $_).Length -eq 0} | select name,distinguishedName | Select-Object | export-csv "$final_local\EmptySecGroups.txt"
-Get-ADGroup -Filter {GroupCategory -eq 'Distribution'} | ?{@(Get-ADGroupMember $_).Length -eq 0} | select name,distinguishedName | Select-Object | export-csv "$final_local\EmptyDistroGroups.txt"
+Get-ADGroup -Filter {GroupCategory -eq 'Security'} | Where-Object{@(Get-ADGroupMember $_).Length -eq 0} | Select-Object name,distinguishedName | Select-Object | export-csv "$final_local\EmptySecGroups.txt"
+Get-ADGroup -Filter {GroupCategory -eq 'Distribution'} | Where-Object{@(Get-ADGroupMember $_).Length -eq 0} | Select-Object name,distinguishedName | Select-Object | export-csv "$final_local\EmptyDistroGroups.txt"
