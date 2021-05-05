1187481fa1966dd4367ff20735ad5d47759cdb99
kellybush <kelly@kellybush.me>
2021-05-05 16:10:06 -0500

    Update Get-GPOs-ReportsAll.ps1

--- Get-GPOs-ReportsAll.ps1
+++ Get-GPOs-ReportsAll.ps1
@@ -5,14 +5,19 @@ $final_local = "$env:userprofile\Desktop\ADData\GPOData\ReportsAll\$date";
 $date = get-date -format M.d.yyyy
 $local = Get-Location;
 
-if (!$local.Equals("C:\")) { Set-Location "C:\" }
+if (!$local.Equals("C:\")) {
+    Set-Location "C:\";
+    if ((Test-Path $final_local) -eq 0) {
+        mkdir $final_local;
+        Set-Location $final_local;
+    }
 
-if ((Test-Path $final_local) -eq 0) {
-    New-item -Path $final_local -ItemType "directory"
-    Set-Location $final_local;
-}
-elseif ((Test-Path $final_local) -eq 1) {
-    Set-Location $final_local
+    ## if path already exists
+    ## DB Connect
+    elseif ((Test-Path $final_local) -eq 1) {
+        Set-Location $final_local;
+        Write-Output $final_local;
+    }
 }
 
 Set-Location $final_local
@@ -22,8 +27,5 @@ $PDC = (Get-ADDomain).PDCEmulator
 
 # Get the reports
 Get-GPOReport -All -Domain $Domain -Server $PDC -ReportType HTML -Path "$final_local\!GPOReportsAll.HTML"
-Get-GPO -All | % {$_.GenerateReport('html') | Out-File "$($_.DisplayName).html"}
-
-
-
+Get-GPO -All | % { $_.GenerateReport('html') | Out-File "$($_.DisplayName).html" }
 
